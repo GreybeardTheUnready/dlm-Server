560cdfad4e87ad94a8620c93359d3641827b79b9
GreybeardTheUnready <github@somerandomname.com>
2019-10-04 10:18:27 +0100

    Updated to accurately relect LIVE as of 041019

--- upload.php
+++ upload.php
@@ -1,44 +1,49 @@
 <?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
-}
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
+error_log("upload.php");
+$targetDir = "uploads";
+
+// First check that we got the file, the whole file and nothing but the file...
+if($_FILES['file']['error'] > 0) {
+	$result = 'Upload failed: ';
+	switch($_FILES['file']['error'])  { 	// These are default PHP Errors
+		case 1: 						// File exceeded the PHP property upload_max_filesize
+		case 2: $result .= "File too big"; break; 		// File has exceeded max_file_size (The hidden element we created on our form)
+		case 3: $result .= 'Incomplete upload'; break;		// File was partially uploaded, not the whole thing
+		case 4: $result .= 'Unknown Error (4)'; break;		// No was file uploaded
+	}
 } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
+	// Check that it is a 'legal' file
+	$isAllowed = array("image/jpg","image/jpeg");
+	if  (!in_array ( $_FILES['file']['type'], $isAllowed )) {
+		$result = "File ".$_FILES['file']['name']." was not an allowed type.<br>";
+		$result .= "Allowed types are jpg, jpeg";
+//error_log($result);
+	} else {
+		// Looks like we have received a valid image file.
+		// So now we need to see if this was invoked by a client upload, or interactively by the user through the console.
+		if ($_POST['source'] == "dlmMobile") {
+			// Client upload so now we extract the chanid and use it to get the Building and Meter.
+			$fn = $_FILES['file']['name'];
+//error_log("File = " . $fn);
+
+			$targetPath = $targetDir . DIRECTORY_SEPARATOR . $fn;
+//error_log("targetPath = " . $targetPath);
+			if(move_uploaded_file($_FILES['file']['tmp_name'], $targetPath)) {
+				$result = $_POST['original'];
+				error_log("DLM Photo uploaded as $fn Into [ ". $targetDir. "]");
+			} else {
+				$result = "Failed to upload: [" . $_FILES['file']['error'] . "]";
+				error_log("DLM Photo upload error: " . $_FILES['file']['error']);
+			}
+
+
+
+		} else {
+			$result = "Failed to upload: File from unknown source";
+			error_log("DLM Photo upload error: File from unknown source");
+		}
+	}
 }
+echo $result;
+
 ?>
